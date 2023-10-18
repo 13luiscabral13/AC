f6b3ed3bbe41b6976a7fdaff0e43be0fc4ad9845
Luís <up202006464@fe.up.pt>
2023-10-18 11:03:31 +0100

    Player Teams

--- change_data.py
+++ change_data.py
@@ -1,18 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('dataset_edited/players_teams_w_percentages_wo_post.csv')
-
-df = df.loc[:, ~df.columns.str.contains('nname')]
+df = pd.read_csv('dataset_edited/new_tester.csv')
 
 
-#df['percentage_pointsFromFieldGoal'] = (df['pointsFromFieldGoal'] / df['points']) * 100
+# Get a list of unique positions
+positions = df['pos'].unique()
 
-#df["percentage_pointsFromFreeThrow"] = (df["ftMade"] / df["points"]) * 100
+# Create a list of colors for the histograms
+colors = ['red', 'green', 'purple', 'orange', 'black', 'pink', 'cyan', 'lavender', 'lime', 'indigo']
+# Create a single plot with 5 histograms, each corresponding to a position
+plt.figure(figsize=(10, 6))
 
-#df["percentage_pointsFromThree"] = (df["threeMade"] / df["points"]) * 100
+for i, position in enumerate(positions):
+    position_data = df[df['pos'] == position]
+    #plt.hist(position_data['ppm'], bins=20, color=colors[i], label=position, alpha=0.5, density=True)
+    sns.kdeplot(position_data['ppm'], color=colors[i], label=position+' density', zorder=2)
 
-df.to_csv('dataset_edited/players_teams_w_percentages_wo_post.csv')
+plt.title('Histograms of PPM for Different Positions')
+plt.xlabel('PPM')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.legend()
+plt.savefig('density_for_ppm')
+plt.show() # HISTOGRAM FOR EVERY POSITION
+
+
+
+
+
